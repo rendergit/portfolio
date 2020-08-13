99b13d74e2c850ba6790fcdeadf581a62b27efe4
rendergit <rendersangma@gmail.com>
2020-08-14 01:40:31 +0530

    added hover

--- pack-js/main.js
+++ pack-js/main.js
@@ -1,1 +1,23 @@
 /*-- main.js --*/
+window.onload = function() {
+	var iblock = document.querySelectorAll('.dmain .dgrid .dimage-block');
+	var icount = iblock.length;
+	var ihover = document.querySelectorAll('dmain .dgrid .doverlay-card');
+	for (var i = 0; i < icount; i++) {
+		iblock[i].addEventListener('mouseover',function() {
+			ihover.style.display = 'block';
+		});
+	} // end of for loop
+}
+
+
+
+
+/*
+|
+|
+|
+|
+|
+*/
+/*-- end of file --*/
