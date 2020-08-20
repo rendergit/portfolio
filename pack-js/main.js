e78d7fc974178640d376118721d886738a17f97c
rendergit <rendersangma@gmail.com>
2020-08-20 17:44:27 +0530

    modified hover script

--- pack-js/main.js
+++ pack-js/main.js
@@ -1,14 +1,16 @@
 /*-- main.js --*/
+//
 window.onload = function() {
-	var iblock = document.querySelectorAll('.dmain .dgrid .dimage-block');
-	var icount = iblock.length;
-	var ihover = document.querySelectorAll('dmain .dgrid .doverlay-card');
-	for (var i = 0; i < icount; i++) {
-		iblock[i].addEventListener('mouseover',function() {
-			ihover.style.display = 'block';
-		});
-	} // end of for loop
+	var iblock = document.querySelectorAll('.dmain .dgrid .dimage');
+	const icount = iblock.length;
+	for(var j = icount; j--;){
+		var itarget = iblock[j].querySelector(' .dover');
+		iblock[j].addEventListener('mouseover', itarget.setAttribute('style', 'visibility:visible;'));
+		iblock[j].addEventListener('mouseout', itarget.setAttribute('style', 'visibility:hidden;'));
+		console.log(j);
+	}
 }
+
 
 
 
